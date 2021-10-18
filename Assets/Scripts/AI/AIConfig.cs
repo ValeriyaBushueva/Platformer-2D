1509697b5c2ae3bcd2328314e38686eb7117ae01
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-10-18 20:40:06 +0300

    Joint and effector

---
+++ Assets/Scripts/AI/AIConfig.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct AIConfig
+{
+    public float Speed;
+    public float MinDistanceToTarget;
+    public Transform[] Waipoints;
+}
